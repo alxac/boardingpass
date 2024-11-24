2f3598e0bbf4dff9acbf57740bbd94c50ba70fa6
alxac <alxac@msn.com>
2024-11-23 21:53:57 -0400

    Ajustes e configuração inicial do projeto

---
+++ src/app/home/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Oi, testando  1 2 3 !</Text>
+    </View>
+  );
+}
+
+
